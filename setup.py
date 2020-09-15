e2effaf2375ddb0c6a03ead4584e42b575db12a5
Rohit Khattar <rohitkh@byu.edu>
2020-09-14 20:42:28 -0600

    Fixes to setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ release_package = 'tethysapp-' + app_package
 # -- Python Dependencies -- #
 dependencies = []
 
+# -- Get Resource File -- #
+resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
+resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
+resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
+
 setup(
     name=release_package,
     version='0.0.4',
